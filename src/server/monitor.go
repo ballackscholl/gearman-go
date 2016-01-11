20cb2d2fbc964088b56fd679853d911eb20cee45
zhouyu <ballackscholl@icloud.com>
2016-01-11 15:14:19 +0800

    fix monitor

--- src/server/monitor.go
+++ src/server/monitor.go
@@ -29,22 +29,22 @@ func registerWebHandler(s *Server, addr string) {
 	m.Get("/goroutine", pprof.Handler("goroutine").ServeHTTP)
 	m.Get("/threadcreate", pprof.Handler("threadcreate").ServeHTTP)
 	m.Get("/status/func", func(params martini.Params) string {
-		e := &Event{tp: getJobStatus, result: createResCh()}
-		s.protoEvtCh <- e
-		return (<-e.result).(string)
-	})
-	m.Get("/status/worker", func(params martini.Params) string {
 		e := &Event{tp: getFuncWorkerStatus, result: createResCh()}
 		s.protoEvtCh <- e
 		return (<-e.result).(string)
 	})
-	m.Get("/status/client", func(params martini.Params) string {
+	m.Get("/status/worker", func(params martini.Params) string {
 		e := &Event{tp: getWorkerStatus, result: createResCh()}
 		s.protoEvtCh <- e
 		return (<-e.result).(string)
 	})
-	m.Get("/status/job", func(params martini.Params) string {
+	m.Get("/status/client", func(params martini.Params) string {
 		e := &Event{tp: getClientStatus, result: createResCh()}
+		s.protoEvtCh <- e
+		return (<-e.result).(string)
+	})
+	m.Get("/status/job", func(params martini.Params) string {
+		e := &Event{tp: getJobStatus, result: createResCh()}
 		s.protoEvtCh <- e
 		return (<-e.result).(string)
 	})
